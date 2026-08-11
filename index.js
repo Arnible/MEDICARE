5b606304d8cc5242adb560c92b68461115eaa6d2
Arnible <160146503+Arnible@users.noreply.github.com>
2026-08-11 23:37:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,263 +1,70 @@
-'use strict';
-
-class QuickLRU {
-	constructor(options = {}) {
-		if (!(options.maxSize && options.maxSize > 0)) {
-			throw new TypeError('`maxSize` must be a number greater than 0');
-		}
-
-		if (typeof options.maxAge === 'number' && options.maxAge === 0) {
-			throw new TypeError('`maxAge` must be a number greater than 0');
-		}
-
-		this.maxSize = options.maxSize;
-		this.maxAge = options.maxAge || Infinity;
-		this.onEviction = options.onEviction;
-		this.cache = new Map();
-		this.oldCache = new Map();
-		this._size = 0;
-	}
-
-	_emitEvictions(cache) {
-		if (typeof this.onEviction !== 'function') {
-			return;
-		}
-
-		for (const [key, item] of cache) {
-			this.onEviction(key, item.value);
-		}
-	}
-
-	_deleteIfExpired(key, item) {
-		if (typeof item.expiry === 'number' && item.expiry <= Date.now()) {
-			if (typeof this.onEviction === 'function') {
-				this.onEviction(key, item.value);
-			}
-
-			return this.delete(key);
-		}
-
-		return false;
-	}
-
-	_getOrDeleteIfExpired(key, item) {
-		const deleted = this._deleteIfExpired(key, item);
-		if (deleted === false) {
-			return item.value;
-		}
-	}
-
-	_getItemValue(key, item) {
-		return item.expiry ? this._getOrDeleteIfExpired(key, item) : item.value;
-	}
-
-	_peek(key, cache) {
-		const item = cache.get(key);
-
-		return this._getItemValue(key, item);
-	}
-
-	_set(key, value) {
-		this.cache.set(key, value);
-		this._size++;
-
-		if (this._size >= this.maxSize) {
-			this._size = 0;
-			this._emitEvictions(this.oldCache);
-			this.oldCache = this.cache;
-			this.cache = new Map();
-		}
-	}
-
-	_moveToRecent(key, item) {
-		this.oldCache.delete(key);
-		this._set(key, item);
-	}
-
-	* _entriesAscending() {
-		for (const item of this.oldCache) {
-			const [key, value] = item;
-			if (!this.cache.has(key)) {
-				const deleted = this._deleteIfExpired(key, value);
-				if (deleted === false) {
-					yield item;
-				}
-			}
-		}
-
-		for (const item of this.cache) {
-			const [key, value] = item;
-			const deleted = this._deleteIfExpired(key, value);
-			if (deleted === false) {
-				yield item;
-			}
-		}
-	}
-
-	get(key) {
-		if (this.cache.has(key)) {
-			const item = this.cache.get(key);
-
-			return this._getItemValue(key, item);
-		}
-
-		if (this.oldCache.has(key)) {
-			const item = this.oldCache.get(key);
-			if (this._deleteIfExpired(key, item) === false) {
-				this._moveToRecent(key, item);
-				return item.value;
-			}
-		}
-	}
-
-	set(key, value, {maxAge = this.maxAge === Infinity ? undefined : Date.now() + this.maxAge} = {}) {
-		if (this.cache.has(key)) {
-			this.cache.set(key, {
-				value,
-				maxAge
-			});
-		} else {
-			this._set(key, {value, expiry: maxAge});
-		}
-	}
-
-	has(key) {
-		if (this.cache.has(key)) {
-			return !this._deleteIfExpired(key, this.cache.get(key));
-		}
-
-		if (this.oldCache.has(key)) {
-			return !this._deleteIfExpired(key, this.oldCache.get(key));
-		}
-
-		return false;
-	}
-
-	peek(key) {
-		if (this.cache.has(key)) {
-			return this._peek(key, this.cache);
-		}
-
-		if (this.oldCache.has(key)) {
-			return this._peek(key, this.oldCache);
-		}
-	}
-
-	delete(key) {
-		const deleted = this.cache.delete(key);
-		if (deleted) {
-			this._size--;
-		}
-
-		return this.oldCache.delete(key) || deleted;
-	}
-
-	clear() {
-		this.cache.clear();
-		this.oldCache.clear();
-		this._size = 0;
-	}
-	
-	resize(newSize) {
-		if (!(newSize && newSize > 0)) {
-			throw new TypeError('`maxSize` must be a number greater than 0');
-		}
-
-		const items = [...this._entriesAscending()];
-		const removeCount = items.length - newSize;
-		if (removeCount < 0) {
-			this.cache = new Map(items);
-			this.oldCache = new Map();
-			this._size = items.length;
-		} else {
-			if (removeCount > 0) {
-				this._emitEvictions(items.slice(0, removeCount));
-			}
-
-			this.oldCache = new Map(items.slice(removeCount));
-			this.cache = new Map();
-			this._size = 0;
-		}
-
-		this.maxSize = newSize;
-	}
-
-	* keys() {
-		for (const [key] of this) {
-			yield key;
-		}
-	}
-
-	* values() {
-		for (const [, value] of this) {
-			yield value;
-		}
-	}
-
-	* [Symbol.iterator]() {
-		for (const item of this.cache) {
-			const [key, value] = item;
-			const deleted = this._deleteIfExpired(key, value);
-			if (deleted === false) {
-				yield [key, value.value];
-			}
-		}
-
-		for (const item of this.oldCache) {
-			const [key, value] = item;
-			if (!this.cache.has(key)) {
-				const deleted = this._deleteIfExpired(key, value);
-				if (deleted === false) {
-					yield [key, value.value];
-				}
-			}
-		}
-	}
-
-	* entriesDescending() {
-		let items = [...this.cache];
-		for (let i = items.length - 1; i >= 0; --i) {
-			const item = items[i];
-			const [key, value] = item;
-			const deleted = this._deleteIfExpired(key, value);
-			if (deleted === false) {
-				yield [key, value.value];
-			}
-		}
-
-		items = [...this.oldCache];
-		for (let i = items.length - 1; i >= 0; --i) {
-			const item = items[i];
-			const [key, value] = item;
-			if (!this.cache.has(key)) {
-				const deleted = this._deleteIfExpired(key, value);
-				if (deleted === false) {
-					yield [key, value.value];
-				}
-			}
-		}
-	}
-
-	* entriesAscending() {
-		for (const [key, value] of this._entriesAscending()) {
-			yield [key, value.value];
-		}
-	}
-
-	get size() {
-		if (!this._size) {
-			return this.oldCache.size;
-		}
-
-		let oldCacheSize = 0;
-		for (const key of this.oldCache.keys()) {
-			if (!this.cache.has(key)) {
-				oldCacheSize++;
-			}
-		}
-
-		return Math.min(this._size + oldCacheSize, this.maxSize);
-	}
+import { UnknownAction } from "../errors.js";
+import { SessionStore } from "./utils/cookie.js";
+import { init } from "./init.js";
+import renderPage from "./pages/index.js";
+import * as actions from "./actions/index.js";
+import { validateCSRF } from "./actions/callback/oauth/csrf-token.js";
+import { skipCSRFCheck } from "./symbols.js";
+export { customFetch, raw, skipCSRFCheck } from "./symbols.js";
+/** @internal */
+export async function AuthInternal(request, authOptions) {
+    const { action, providerId, error, method } = request;
+    const csrfDisabled = authOptions.skipCSRFCheck === skipCSRFCheck;
+    const { options, cookies } = await init({
+        authOptions,
+        action,
+        providerId,
+        url: request.url,
+        callbackUrl: request.body?.callbackUrl ?? request.query?.callbackUrl,
+        csrfToken: request.body?.csrfToken,
+        cookies: request.cookies,
+        isPost: method === "POST",
+        csrfDisabled,
+    });
+    const sessionStore = new SessionStore(options.cookies.sessionToken, request.cookies, options.logger);
+    if (method === "GET") {
+        const render = renderPage({ ...options, query: request.query, cookies });
+        switch (action) {
+            case "callback":
+                return await actions.callback(request, options, sessionStore, cookies);
+            case "csrf":
+                return render.csrf(csrfDisabled, options, cookies);
+            case "error":
+                return render.error(error);
+            case "providers":
+                return render.providers(options.providers);
+            case "session":
+                return await actions.session(options, sessionStore, cookies);
+            case "signin":
+                return render.signin(providerId, error);
+            case "signout":
+                return render.signout();
+            case "verify-request":
+                return render.verifyRequest();
+            case "webauthn-options":
+                return await actions.webAuthnOptions(request, options, sessionStore, cookies);
+            default:
+        }
+    }
+    else {
+        const { csrfTokenVerified } = options;
+        switch (action) {
+            case "callback":
+                if (options.provider.type === "credentials")
+                    // Verified CSRF Token required for credentials providers only
+                    validateCSRF(action, csrfTokenVerified);
+                return await actions.callback(request, options, sessionStore, cookies);
+            case "session":
+                validateCSRF(action, csrfTokenVerified);
+                return await actions.session(options, sessionStore, cookies, true, request.body?.data);
+            case "signin":
+                validateCSRF(action, csrfTokenVerified);
+                return await actions.signIn(request, cookies, options);
+            case "signout":
+                validateCSRF(action, csrfTokenVerified);
+                return await actions.signOut(cookies, sessionStore, options);
+            default:
+        }
+    }
+    throw new UnknownAction(`Cannot handle action: ${action}`);
 }
-
-module.exports = QuickLRU;
